27f05f78f70a97a4647e737b89aac327b237beca
Roland Smith <rsmith@xs4all.nl>
2025-01-14 21:49:19 +0100

    Removed unused variable.

--- base64.c
+++ base64.c
@@ -4,7 +4,7 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-01-13 21:22:40 +0100
-// Last modified: 2025-01-14T20:41:11+0100
+// Last modified: 2025-01-14T21:48:32+0100
 
 #include <stdio.h>
 #include <stdint.h>
@@ -24,7 +24,6 @@ int b64encode(char *in, uint32_t inlen, char *out, uint32_t outlen)
     unsigned char buf[xlen];
     memset(buf, '=', xlen);
     memcpy(buf, in, inlen);
-    const uint32_t k[4] = {0xfc0000, 0x3f000, 0xfc0, 0x3f};
     for (int32_t j = 0, olen = 0; j < xlen; j+=3, olen+=4) {
       unsigned char obuf[4] = {0};
       uint32_t t = (buf[j]<<16)|(buf[j+1]<<8)|(buf[j+2]);
