09b5f32fe2e8ae90ee088a3836108482297623a8
Roland Smith <rsmith@xs4all.nl>
2026-03-16 00:21:47 +0100

    Add some bling to the tests. ;)

--- b64test.c
+++ b64test.c
@@ -5,7 +5,7 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-01-18 20:25:21 +0100
-// Last modified: 2026-03-14T17:06:18+0100
+// Last modified: 2026-03-16T00:20:14+0100
 
 #include "base64.h"
 #include <stdio.h>
@@ -40,6 +40,8 @@ int main(void)
     "QWJhY2FkYWJyYQ==",
     "QWJhY 2FkYWJyYQ=="
   };
+  int failcount = 0;
+  puts(PURPLE"Running tests for b64encode"RESET);
   for (int32_t j = 0; j < 5; j++) {
     char out[64] = {0};
     b64encode(in[j], strlen(in[j]), out, 63);
@@ -47,9 +49,10 @@ int main(void)
       printf(GREEN"PASSED:"RESET" b64encode(\"%s\") == \"%s\"\n", in[j], out);
     } else {
       printf(RED"FAILED:"RESET" b64encode(\"%s\") != \"%s\"\n", in[j], out);
+      failcount++;
     }
   }
-  puts("-----");
+  puts(PURPLE"Running tests for b64decode"RESET);
   for (int32_t j = 0; j < 6; j++) {
     char out[64] = {0};
     b64decode(expected[j], strlen(expected[j]), out, 63);
@@ -57,7 +60,14 @@ int main(void)
       printf(GREEN"PASSED:"RESET" b64decode(\"%s\") == \"%s\"\n", expected[j], out);
     } else {
       printf(RED"FAILED:"RESET" b64decode(\"%s\") != \"%s\"\n", expected[j], out);
+      failcount++;
     }
+  }
+  puts(PURPLE"*** Result ***"RESET);
+  if (failcount == 0) {
+    puts(GREEN"+++ All tests PASSED! +++"RESET);
+  } else {
+    printf("\033[1;31m--- %d failing tests.---\033[0m\n", failcount);
   }
   return 0;
 }
