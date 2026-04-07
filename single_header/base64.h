7b4d44496550632adb95fd1a71af2ff4f8718bed
Roland Smith <rsmith@xs4all.nl>
2026-04-07 19:05:13 +0200

    Make compatible with C++.

--- single_header/base64.h
+++ single_header/base64.h
@@ -5,14 +5,21 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-01-18 20:22:18 +0100
-// Last modified: 2025-08-29T00:26:50+0200
+// Last modified: 2026-04-07T19:03:44+0200
 
 #pragma once
-
 #include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern void b64encode(const char *in, uint32_t inlen, char *out, uint32_t outlen);
 extern int b64decode(const char *in, uint32_t inlen, char *out, uint32_t outlen);
+
+#ifdef __cplusplus
+}
+#endif
 
 #ifdef BASE64_IMPLEMENTATION
 #include <string.h>  // for memset, memcpy, strnlen
