1d730aae4ee4a7f87f9ec349b87524e11d0625a5
Roland Smith <rsmith@xs4all.nl>
2026-04-07 08:22:10 +0200

    Generate single header library.

--- Makefile
+++ Makefile
@@ -1,19 +1,23 @@
-# file: Makefile
 # vim:fileencoding=utf-8:ft=make
-#
-# Author: R.F. Smith <rsmith@xs4all.nl>
-# Created: 2025-01-14T21:43:09+0100
-# Last modified: 2026-02-22T01:54:54+0100
+.MAKEFLAGS: -j C
 
-all: b64test
+all: b64test single_header/base64.h
 
 CFLAGS = -pipe -std=c11 -g3 -Wall -Wextra -Wstrict-prototypes -Wpedantic \
                 -Wshadow -Wmissing-field-initializers -Wpointer-arith
 
 SRCS = b64test.c base64.c
 
-b64test: $(SRCS)  ## Compile test program (default)
-	$(CC) $(CFLAGS) -o b64test  $(SRCS)
+b64test: $(SRCS) base64.h  ## Compile test program (default)
+	$(CC) $(CFLAGS) -o b64test $(SRCS)
+
+single_header/base64.h: base64.c base64.h  ## Build single header library (POSIX only).
+	cp base64.h single_header/base64.h
+	echo "" >>single_header/base64.h
+	echo "#ifdef BASE64_IMPLEMENTATION" >>single_header/base64.h
+	tail -n +11 base64.c >>single_header/base64.h
+	echo "" >>single_header/base64.h
+	echo "#endif // BASE64_IMPLEMENTATION" >>single_header/base64.h
 
 .PHONY: style
 style:  ## Reformat source code using astyle.
@@ -25,7 +29,7 @@ tidy:  ## Run static code checker clang-tidy.
 
 .PHONY: clean
 clean:  ## Remove generated files
-	rm -f *.o b64test b64test.exe
+	rm -f *.o b64test b64test.exe single_header/base64.h
 
 .PHONY: help
 help:  ## List available commands
---
+++ single_header/base64.h
@@ -0,0 +1,106 @@
+// file: base64.h
+// vim:fileencoding=utf-8:ft=c:tabstop=2
+// This is free and unencumbered software released into the public domain.
+//
+// Author: R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: Unlicense
+// Created: 2025-01-18 20:22:18 +0100
+// Last modified: 2025-08-29T00:26:50+0200
+
+#pragma once
+
+#include <stdint.h>
+
+extern void b64encode(const char *in, uint32_t inlen, char *out, uint32_t outlen);
+extern int b64decode(const char *in, uint32_t inlen, char *out, uint32_t outlen);
+
+#ifdef BASE64_IMPLEMENTATION
+#include <string.h>  // for memset, memcpy, strnlen
+
+static const char B64[65] =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+void b64encode(const char *in, uint32_t inlen, char *out, uint32_t outlen)
+{
+  uint32_t xlen = 3 - (inlen % 3), after;
+  if (xlen == 3) {
+    xlen = 0;
+  }
+  after = xlen;
+  xlen += inlen;
+  unsigned char buf[xlen];
+  memset(buf, 0, xlen);
+  memset(out, 0, outlen);
+  memcpy(buf, in, inlen);
+  char *ix = out;
+  for (uint32_t j = 0, olen = 0; j < xlen; j+=3, olen+=4) {
+    unsigned char obuf[4] = {0};
+    uint32_t t = (buf[j]<<16)|(buf[j+1]<<8)|(buf[j+2]);
+    obuf[0] = B64[(t&0xfc0000)>>18];
+    obuf[1] = B64[(t&0x3f000)>>12];
+    obuf[2] = B64[(t&0xfc0)>>6];
+    obuf[3] = B64[t&0x3f];
+    if (olen <= outlen - 4) {
+      memcpy(ix, obuf, 4);
+      ix += 4;
+    }
+  }
+  uint32_t offs = strnlen(out, outlen);
+  switch (after) {
+    case 1:
+      out[offs-1] = '=';
+      break;
+    case 2:
+      out[offs-2] = '=';
+      out[offs-1] = '=';
+      break;
+  };
+}
+
+
+// In Python:
+// invB64 = [B64.index(chr(j)) if chr(j) in B64 else -1 for j in range(128)]
+static const char invB64[128] = {
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+  52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+  -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+  18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27,
+  28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+  44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1
+};
+
+int b64decode(const char *in, uint32_t inlen, char *out, uint32_t outlen)
+{
+  int ix = 0;
+  uint32_t outcnt = 0;
+  unsigned char obuf[5] = {0};
+  char *p = out;
+  for (uint32_t j = 0; j < inlen; j++) {
+    int cur = (unsigned char)in[j];
+    if (cur == 61) {
+      // Filler
+      obuf[ix++] = 0;
+    } else if (cur < 0 || invB64[cur] == -1) {
+      continue; // Ignore illegal characters.
+    } else {
+      obuf[ix++] = invB64[cur];
+    }
+    if (ix == 4) {
+      uint32_t k = (obuf[0]<<18)|(obuf[1]<<12)|(obuf[2]<<6)|obuf[3];
+      memset(obuf, 0, 5);
+      ix = 0;
+      unsigned char tmp[3] = {(k&0xff0000)>>16, (k&0xff00)>>8, k&0xff};
+      for (int32_t q = 0; q < 3; q++) {
+        *p++ = tmp[q];
+        if (++outcnt >= outlen) {
+          return 0;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+#endif // BASE64_IMPLEMENTATION
